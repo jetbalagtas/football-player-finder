00ab1cca5167525bd329d7c8a933666c5b8f35c2
Jet Balagtas <jetbalagtas@gmail.com>
2019-12-13 12:48:40 -0500

    axios get json, pass props, render data

--- src/components/FootballPlayers/FootballPlayers.js
+++ src/components/FootballPlayers/FootballPlayers.js
@@ -9,45 +9,34 @@ import Paper from '@material-ui/core/Paper';
 
 import classes from './FootballPlayers.module.css';
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
+export default function FootballPlayers(props) {
+  const players = props.players;
 
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
-
-const FootballPlayers = () => (
-  <Paper className={classes.Root}>
-    <Table className={classes.Table} aria-label="simple table">
-      <TableHead>
-        <TableRow>
-          <TableCell>Dessert (100g serving)</TableCell>
-          <TableCell align="right">Calories</TableCell>
-          <TableCell align="right">Fat&nbsp;(g)</TableCell>
-          <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-          <TableCell align="right">Protein&nbsp;(g)</TableCell>
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {rows.map(row => (
-          <TableRow key={row.name}>
-            <TableCell component="th" scope="row">
-              {row.name}
-            </TableCell>
-            <TableCell align="right">{row.calories}</TableCell>
-            <TableCell align="right">{row.fat}</TableCell>
-            <TableCell align="right">{row.carbs}</TableCell>
-            <TableCell align="right">{row.protein}</TableCell>
+  return (
+    <Paper className={classes.Root}>
+      <Table className={classes.Table} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Player</TableCell>
+            <TableCell align="right">Position</TableCell>
+            <TableCell align="right">Team</TableCell>
+            <TableCell align="right">Jersey Number</TableCell>
+            <TableCell align="right"></TableCell>
           </TableRow>
-        ))}
-      </TableBody>
-    </Table>
-  </Paper>
-);
-
-export default FootballPlayers;
+        </TableHead>
+        <TableBody>
+          {players.map(row => (
+            <TableRow key={row.name}>
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.position}</TableCell>
+              <TableCell align="right">{row.nationality}</TableCell>
+              <TableCell align="right">{row.jerseyNumber}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </Paper>
+  );
+}
--- src/containers/FootballPlayerFinder/FootballPlayerFinder.js
+++ src/containers/FootballPlayerFinder/FootballPlayerFinder.js
@@ -1,14 +1,29 @@
-import  React, { Component } from "react";
+import  React, { Component } from 'react';
+import axios from 'axios';
 
 import Aux from '../../hoc/Aux';
 import FootballPlayers from '../../components/FootballPlayers/FootballPlayers';
 
 class FootballPlayerFinder extends Component {
+  state = {
+    players: []
+  }
+
+  componentDidMount () {
+    axios.get('https://football-players-b31f2.firebaseio.com/players.json')
+      .then(response => {
+        this.setState({players: response.data});
+        console.log(response);
+      });
+  }
+
   render () {
+    const players = this.state.players;
+    
     return (
       <Aux>
         <div>Filter/Search Controls</div>
-        <FootballPlayers />
+        <FootballPlayers players={players}/>
       </Aux>
     );
   }
